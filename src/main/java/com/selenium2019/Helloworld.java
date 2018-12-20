9a72dcebcbe4c22c620b2761885881bdbdbc6bd4
amitechno <vit.amitsharma@gmail.com>
2018-12-20 17:36:17 +0530

    Update Helloworld.java

--- src/main/java/com/selenium2019/Helloworld.java
+++ src/main/java/com/selenium2019/Helloworld.java
@@ -3,7 +3,7 @@ package com.selenium2019;
 public class Helloworld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello World! amit");
 
 	}
 
